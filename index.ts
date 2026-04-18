97fe9cc310d984fa7f95297a86f0d02a561bed02
Rei WuZen <reiwuzen@gmail.com>
2026-04-18 19:35:23 +0530

    feat: add exports for useWorkspace, useTabs, usePage hooks and ActiveTab type

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { useWorkspace } from "./src/hooks/useWorkspace";
+export { useTabs } from "./src/hooks/useTabs";
+export { usePage } from "./src/hooks/usePage";
+export type { ActiveTab } from "./src/hooks/useTabs";
